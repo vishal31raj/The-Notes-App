d5e371f4562cd85b45db564b44e837d0ae1df63c
vishal31raj <vraj07909@gmail.com>
2018-12-23 09:23:51 +0530

    My very first nodejs app

---
+++ app.js
@@ -0,0 +1,73 @@
+const notes = require('./notes.js');
+const yargs = require('yargs');
+
+const titleOptions = {
+    describe : 'Title of note',
+    demand : true,
+    alias : 't'
+};
+
+const bodyOptions = {
+    describe : 'Body of note',
+    demand : true,
+    alias : 'b'
+};
+
+const argv = yargs
+    .command('add', 'Adding a new note', {
+        title : titleOptions,
+        body : bodyOptions
+    })
+    .command('list', 'List all notes')
+    .command('read', 'Read a note', {
+        title : titleOptions
+    })
+    .command('remove', 'Remove a note', {
+        title : titleOptions
+    })
+    .help()
+    .argv;
+
+var command = argv._[0];
+
+if(command==='add'){
+    var note = notes.addNote(argv.title, argv.body);
+    if(note) {
+        console.log("Note Created.");
+        notes.logNote(note);
+    }
+    else {
+        console.log("Note title taken.");
+    }
+} 
+
+else if(command==='list'){
+    var allNotes = notes.getAll();
+    console.log(`Printing ${allNotes.length} note(s).`);
+    allNotes.forEach((note) => notes.logNote(note));
+} 
+
+else if(command==='read'){
+    var note = notes.getNote(argv.title);
+    if(note) {
+        console.log('Note found.');
+        notes.logNote(note);
+    }
+    else {
+        console.log('Note not found.');
+    }
+} 
+
+else if(command==='remove'){
+    var note = notes.removeNote(argv.title);
+    if(note===true) {
+        console.log('Note was Removed.');
+    }
+    else {
+        console.log('Note not found.')
+    }
+} 
+
+else{
+    console.log('Command not recognised!');
+}
